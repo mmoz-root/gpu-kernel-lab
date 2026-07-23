88a3ff8ea150607c17a874dd6c471af02b24da60
mmoz-root <ozbey.muhammedtr@gmail.com>
2026-07-23 11:07:40 +0300

    cuda impl

--- kernels/vector-add/cuda_impl.cu
+++ kernels/vector-add/cuda_impl.cu
@@ -4,70 +4,81 @@
 
 // simple kernel func to add elements of two vectors
 __global__ 
-void add(int n, float *sum, float *x, float *y) {
-    for(int i=0; i<n; i++) {
-        sum[i] = x[i] + y[i];
+void add_basic(const float *a, const float *b, float *c, int n) {
+    
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    if(index < n) {
+        c[index] = a[index] + b[index];
+
     }
+}
+void launch_add_basic(const float *a, const float *b, float *c, int n) {
+    constexpr int threads_per_block = 256;
+    int blocks = (n + threads_per_block - 1) / threads_per_block;
+    add_basic <<<blocks, threads_per_block>>>(a, b, c, n);
 }
 
-__global__
-void add_2(int n, float* x, float* y) {
-    int idx = threadIdx.x;
-    int stride = blockDim.x;
-    for(int i = idx; i < n; i+=stride) {
-        y[i] = x[i] + y[i];
-    }
-}
+
 
 // the first parameter of the execution configuration specifies the number of thread blocks. 
 // Together, the blocks of parallel threads make up what is known as the grid
 __global__
-void add_3(int n, float* x, float *y) {
+void add_gridnstride(const float* x, const float *y, float *z, int n) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     int stride = blockDim.x * gridDim.x;
     for(int i=idx; i<n; i+=stride){
-        y[i] = x[i] + y[i];
+        z[i] = x[i] + y[i];
     }
+}
+void launch_add_gridnstride(const float *x, const float *y, float *z, int n) {
+    constexpr int threads_per_block = 256;
+    cudaDeviceProp properties;
+    cudaGetDevice(&device);
+    cudaGetDeviceProperties(&properties, device);
+
+    int blocks = properties.multiProcessorCount * 4;
+    add_gridnstride <<<blocks, threads_per_block>>>(x, y, z, n);
 }
 
 
-int main() {
-    int N = 1<<20;
-    float *x, *y;
 
-    cudaMallocManaged(&x, N*sizeof(float));
-    cudaMallocManaged(&y, N*sizeof(float));
+//vectorized version using float4
+__global__
+void add_float4(const float* x, const float *y, float *z, int n) {
+    int index = blockDim.x * blockIdx.x + threadIdx.x;
+    int vector_count / 4;
 
-    // Prefetch the x and y arrays to the GPU
-    cudaMemPrefetchAsync(x, N*sizeof(float), 0, 0);
-    cudaMemPrefetchAsync(y, N*sizeof(float), 0, 0);
+    if (index < vector_count) {
+        const float4* a4 = reinterpret_cast<const float4*>(a);
+        const float4* b4 = reinterpret_cast<const float4*>(b);
+        float4* c4 = reinterpret_cast<float4*>(c);
 
-    for(int i=0; i<N; i++) {
-        x[i] = 1.0f;
-        y[i] = 2.0f;
+        float4 av = a4[index];
+        float4 bv = b4[index];
+
+        c4[index] = make_float4(
+            av.x + bv.x,
+            av.y + bv.y,
+            av.z + bv.z,
+            av.w + bv.w
+        );
+        int tail_start = vector_count * 4;
+        int tail_size = n - tail_start;
+
+        if (index < tail_size) {
+            int tail_index = tail_start + index;
+            c[tail_index] = a[tail_index] + b[tail_index];
+        }
     }
-
-    //blocks of threads that are a mult of 32 in size
-    add <<<1, 1>>>(N, sum, x, y); // 1 parallel thread
-
-    add_2 <<<1, 256>>>(N, x, y); // 256 parallel threads
-
-    int blockSize = 256;
-    int numBlocks = (N + blockSize-1) / blockSize;
-    add_3 <<<numBlocks, blockSize>>>(N, x, y);
-
-
-    // I need the CPU to wait until the kernel is done before it accesses the results 
-    // (because CUDA kernel launches don’t block the calling CPU thread)
-    cudaDeviceSynchronize();
-
-    float maxError = 0.0f;
-    for(int i=0; i<N; i++) {
-        maxError = fmax(maxError, fabs(y[i] - 3.0f));
-    }
-    std::cout << "Max Error: " << maxError << std::endl;
-
-    cudaFree(x);
-    cudaFree(y);
-    return 0;
 }
+void launch_add_float4(const float *x, const float *y, float *z, int n) {
+    constexpr int threads_per_block = 256;
+    int vector_count = n/4;
+    int work_items = vector_count > 0 ? vector_count : 1;
+    int blocks = (work_items + threads_per_block - 1) / threads_per_block;
+    add_float4 <<<blocks, threads_per_block>>>(x, y, z, n);
+}
+
+
+
+int main() {}
